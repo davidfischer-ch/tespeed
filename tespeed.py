639010100242c717125abf796ba424d0106331a2
David Fischer <david.fischer.ch@gmail.com>
2014-05-24 13:20:29 +0200

    Fix wording + use underscores

--- tespeed.py
+++ tespeed.py
@@ -360,17 +360,17 @@ def main(args):
         set_proxy(typ=socks.PROXY_TYPE_SOCKS5 if args.use_proxy == 5 else socks.PROXY_TYPE_SOCKS4, host=args.proxy_host,
                   port=args.proxy_port)
 
-    if args.listservers:
+    if args.list_servers:
         args.store = True
 
     log = Log(suppress=args.suppress, store=args.store)
-    if not args.listservers and args.server == '' and not args.store:
+    if not args.list_servers and args.server == '' and not args.store:
         log.debug('Getting ready. Use parameter -h or --help to see available features.\n')
     else:
         log.debug('Getting ready\n')
     try:
-        tespeed = TeSpeed(server=args.listservers and 'list-servers' or args.server, num_servers=args.servercount,
-                          num_top=args.listservers, unit=args.unit, chunk_size=args.chunksize, log=log)
+        tespeed = TeSpeed(server=args.list_servers and 'list-servers' or args.server, num_servers=args.server_count,
+                          num_top=args.list_servers, unit=args.unit, chunk_size=args.chunk_size, log=log)
         tespeed.run_tests()
     except (KeyboardInterrupt, SystemExit):
         log.debug('\nSpeed test stopped.\n')
@@ -379,17 +379,17 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='TeSpeed, CLI SpeedTest.net')
 
     parser.add_argument('server', nargs='?', type=str, default='', help='Use the specified server for testing (skip checking for location and closest server).')
-    parser.add_argument('-ls', '--list-servers', dest='listservers', nargs='?', default=0, const=10, help='List the servers sorted by distance, nearest first. Optionally specify number of servers to show.')
+    parser.add_argument('-ls', '--list-servers', dest='list_servers', nargs='?', default=0, const=10, help='List the servers sorted by distance, nearest first. Optionally specify number of servers to show.')
     parser.add_argument('-w', '--csv', dest='store', action='store_true', help='Print CSV formated output to STDOUT.')
     parser.add_argument('-s', '--suppress', dest='suppress', action='store_true', help='Suppress debugging (STDERR) output.')
     parser.add_argument('-mib', '--mebibit', dest='unit', action='store_true', help='Show results in mebibits.')
-    parser.add_argument('-n', '--server-count', dest='servercount', nargs='?', default=1, const=1, help='Specify how many different servers should be used in paralel. (Default: 1) (Increase it for >100Mbit testing.)')
+    parser.add_argument('-n', '--server-count', dest='server_count', nargs='?', default=1, const=1, help='Specify how many different servers should be used in parallel. (Default: 1) (Increase it for >100Mbit testing.)')
 
     parser.add_argument('-p', '--proxy', dest='use_proxy', type=int, nargs='?', const=4, help='Specify 4 or 5 to use SOCKS4 or SOCKS5 proxy.')
     parser.add_argument('-ph', '--proxy-host', dest='proxy_host', type=str, nargs='?', default='127.0.0.1', help='Specify socks proxy host. (Default: 127.0.0.1)')
     parser.add_argument('-pp', '--proxy-port', dest='proxy_port', type=int, nargs='?', default=9050, help='Specify socks proxy port. (Default: 9050)')
 
-    parser.add_argument('-cs', '--chunk-size', dest='chunksize', nargs='?', type=int, default=10240, help='Specify chunk size after wich tespeed calculates speed. Increase this number 4 or 5 times if you use weak hardware like RaspberryPi. (Default: 10240)')
+    parser.add_argument('-cs', '--chunk-size', dest='chunk_size', nargs='?', type=int, default=10240, help='Specify chunk size after which tespeed calculates speed. Increase this number 4 or 5 times if you use weak hardware like RaspberryPi. (Default: 10240)')
 
     #parser.add_argument('-i', '--interface', dest='interface', nargs='?', help='If specified, measures speed from data for the whole network interface.')
 
